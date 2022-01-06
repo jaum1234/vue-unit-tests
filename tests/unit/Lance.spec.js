2c94b503d2808189c6c3cbcd00880aafa47f5726
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-01-06 07:02:35 -0300

    testando criaçao do componente, se input é encontrado dentro do componente e se a emiçao de um lance está ocorrenco conforme o esperado

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -1,7 +1,44 @@
 import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
 
-test('nao aceita lances com valores menores que zero', () => {
+test('deve criar o componente Lance', () => {
   const wrapper = mount(Lance)
   expect(wrapper).toBeTruthy()
+})
+
+test('deve encontrar um input em Lance', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  expect(input).toBeTruthy()
+})
+
+test('nao aceita lances com valores menores que zero', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(-100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toBeUndefined()
+})
+
+test('emite um lance quando o valor é maior do que zero', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toHaveLength(1)
+})
+
+test('deve emitir o valor esperado de um lance válido', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  // [
+  //    [100]
+  // ]
+  const lance = parseInt(lancesEmitidos[0][0])
+  expect(lance).toBe(100)
 })
